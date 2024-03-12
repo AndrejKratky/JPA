392e39c93ab10df642e94286b89197c8f70d750c
AndrejKratky <kratky4@stud.uniza.sk>
2024-03-12 21:09:44 +0100

    employee, main, persistence

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setfFirstName("Adam");
+        employee.setlLastName("Kosta");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
 }
